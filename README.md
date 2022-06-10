9e42251c0338ae536c759da46a435a975fc761c9
Gustavo Calasans <106408467+GustavoKoreiaa@users.noreply.github.com>
2022-06-10 19:28:34 +0000

    adicionando comandos em Python

--- README.md
+++ README.md
@@ -1,9 +1,6 @@
-# Praticando estruturas no python.
-##  fizemos exercicios praticos de estrutura de decisão.
-2 exercícios.
-## fizemos exercícios praticos de estrutura de repetição.
-2 exercicios
-### trabalho realizado no replit e enviado ao github
-
-***
-# Gustavo e Gabriel
+# Praticando no Python
+:abacus: As estrutura de decisão permitem alterar o cursor do fluxo de execução de um programa, de acorodo com o valor.
+## Estrutura de Repetição 
+:closed_book: A estrutura de repetiçao é utilizada para executar de comandos varias vezes.
+  ## Função
+:pencil: Aprendemos que função é pequenos trechos de código reutizaveis. E que elas permitem dar um nome a um bloco de comando e execultar esse bloco, a partir de qualquer lugar do programa.
--- main.py
+++ main.py
@@ -1,5 +1,33 @@
+from Funções import hello
+hello()
+from Funções import maior
+maior(4,7)
+from Funções import soma
+
+total = 10
+
+soma(3,5)
+print("Total principal = ",total)
+
+import Funções
+s = Funções.soma3(3,5)
+print("Soma = ",s)
+
+j = Funções.calcula_juros(500)
+print("O juros é = ", j)
+
+print("Resposta do Exercício 01\n")
+from Ex_Funções import linha
+linha(50)
+print("\nResposta do Exercício 02\n")
+
+from Ex_Funções import imprime_lista
+L = [3,6,8,9,10,9,8,4]
+imprime_lista(L)
+
+print("\n\n")
 __author__ = "Gustavo e Gabriel"
-import Estrutura_de_decisão
+'''import Estrutura_de_decisão
 print(Estrutura_de_decisão)
 
 import Ex_Estru_decisão
@@ -9,4 +37,4 @@ import Estrutura_de_repetição
 print(Estrutura_de_repetição)
 
 import Ex_Estru_repetição
-print(Ex_Estru_repetição)
+print(Ex_Estru_repetição)'''
